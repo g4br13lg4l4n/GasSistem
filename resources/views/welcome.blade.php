03293c082d15987a70c11eb3c85ea9f870448931
g4br13lg4l4n <gabriel.galan92@gmail.com>
2018-02-18 23:59:34 -0600

    prepare to update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,12 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>Sistema GasAppLP</title>
         <link rel="stylesheet" href="css/app.css">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="robots" content="noindex">
+        <meta name="googlebot" content="noindex">
+        <meta name="robots" content="noindex, nofollow">
     </head>
     <body>
         <div id="app">    
